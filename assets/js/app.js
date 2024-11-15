8b7e0aa670b312a95fb3a7e3b07e700923fdbe52
AmitSheoran-557 <amit22082006@gmail.com>
2024-11-15 12:32:51 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+new Typed("#typed-text", {
+    strings: ["Awesome Websites!", "Creative Designs!", "Tailwind + JS!"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
+// ==================== color box js =========================
+function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function generatePalette() {
+    const palette = document.getElementById("palette");
+    palette.innerHTML = "";
+    for (let i = 0; i < 8; i++) {
+        const color = getRandomColor();
+        const colorBox = document.createElement("div");
+        colorBox.style.backgroundColor = color;
+        colorBox.classList.add("p-10", "rounded-md", "shadow-xl");
+        const para = document.createElement("p");
+        para.classList.add("text-black", "font-bold", "mt-2");
+        para.textContent = color;
+        colorBox.appendChild(para);
+        palette.appendChild(colorBox);
+    }
+}
+
+generatePalette();
